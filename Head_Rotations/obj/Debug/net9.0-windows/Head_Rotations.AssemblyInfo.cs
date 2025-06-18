ecf3b65ceaeb7a43aa3944e3c98834710c0ec0d5
Benjamin Jersch <bejersch.work@gmail.com>
2025-06-18 12:14:26 +0300

    Build

--- Head_Rotations/obj/Debug/net9.0-windows/Head_Rotations.AssemblyInfo.cs
+++ Head_Rotations/obj/Debug/net9.0-windows/Head_Rotations.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Head")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9444c12debb5915ba4ee212b3e6707a9bfe210ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e435c070df29fa118e1db8586fc7bfc160ea010a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Head_Rotations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Head_Rotations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
